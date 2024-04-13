b4819732166642ccf0879c414ff671f767495b7f
Mert Ali Bayram <mertali.bayram01@dogaokullari.k12.tr>
2024-04-13 20:14:04 +0300

    this doesnt feel like c#

--- WarheadRadiation/EventHandler.cs
+++ WarheadRadiation/EventHandler.cs
@@ -9,36 +9,27 @@ namespace WarheadRadiation
 {
     public class EventHandler
     {
-        private static CoroutineHandle CheckRoomHandle;
-        private static Config _config = Plugin.Instance.Config;
-        private static int timer = _config.Seconds;
+        private static CoroutineHandle _checkRoomHandle;
+        private static readonly Config Config = Plugin.Instance.Config;
+        private static int _timer = Config.Seconds;
         public static void Spawned(SpawnedEventArgs ev)
         {
-            if (!CheckRoomHandle.IsRunning)
-            {
-                CheckRoomHandle = Timing.RunCoroutine(CheckRoom(ev.Player));
-            }
+            if (!_checkRoomHandle.IsRunning)
+                _checkRoomHandle = Timing.RunCoroutine(CheckRoom(ev.Player)); 
         }
 
-        public static IEnumerator<float> CheckRoom(Player player)
+        private static IEnumerator<float> CheckRoom(Player player)
         {
             yield return Timing.WaitForSeconds(1);
             if (player.CurrentRoom?.Type is RoomType.HczNuke)
-            {
-                timer--;
-            }
+                _timer--;
             else
-            {
-                timer = _config.Seconds;}
+                _timer = Config.Seconds;
 
-            if (timer <= 0)
-            {
+            if (_timer <= 0)
                 player.EnableEffect(EffectType.Decontaminating);
-            }
             else
-            {
-                player.DisableEffect(EffectType.Decontaminating);
-            }
+                player.DisableEffect(EffectType.Decontaminating); 
         }
     }
 }
